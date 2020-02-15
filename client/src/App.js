ba090292e2e6eeee36ed6d7e38d1498fe54dcd08
Danny <mr.danny951008@gmail.com>
2020-02-15 20:54:27 +0900

    added Link to Launch Details

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import ApolloClient from "apollo-boost";
 import {ApolloProvider} from "react-apollo";
 import logo from "./spaceXLogo.png";
 import Launches from "./components/Launches";
+import Launch from "./components/Launch";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql"
@@ -13,14 +15,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <ApolloProvider client={client}>
-      <div className="container">
-        <img
-          src={logo}
-          alt="There should be logo"
-          style={{width: 300, height: 50, display: "block", margin: "auto"}}
-        />
-        <Launches />
-      </div>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="There should be logo"
+            style={{width: 300, height: 50, display: "block", margin: "auto"}}
+          />
+          {/* <Launches /> */}
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
     </ApolloProvider>
   );
 }
